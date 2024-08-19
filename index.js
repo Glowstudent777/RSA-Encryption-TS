d165ad268e60895701ce33c9b7c994ee41fc0461
Glowstudent777 <cameronmenken7@gmail.com>
2024-08-19 12:59:33 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const crypto = require('crypto');
+
+const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', { modulusLength: 4096 });
+
+console.log(
+    publicKey.export({
+        type: 'pkcs1',
+        format: 'pem'
+    }),
+    privateKey.export({
+        type: 'pkcs1',
+        format: 'pem'
+    })
+);
+
+function encryptData(data) {
+    return crypto.publicEncrypt({
+        key: publicKey,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+        oaepHash: 'sha256',
+    }, Buffer.from(data))
+}
+
+function decryptData(encryptedData) {
+    return crypto.privateDecrypt({
+        key: privateKey,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+        oaepHash: 'sha256',
+    }, encryptedData)
+}
+
+function signData(data) {
+    return crypto.sign('sha256', Buffer.from(data), {
+        key: privateKey,
+        padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+    })
+}
+
+function verifyData(data, signature) {
+    return crypto.verify('sha256', Buffer.from(data), {
+        key: publicKey,
+        padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+    }, signature)
+}
+
+const data = 'Hello, World!';
+const encryptedData = encryptData(data);
+const decryptedData = decryptData(encryptedData);
+const signature = signData(data);
+const isVerified = verifyData(data, signature);
+
+console.log('Data:', data);
+console.log('Encrypted Data:', encryptedData.toString('base64'));
+console.log('Decrypted Data:', decryptedData.toString());
+console.log('Signature:', signature.toString('base64'));
+console.log('Is Verified:', isVerified);
